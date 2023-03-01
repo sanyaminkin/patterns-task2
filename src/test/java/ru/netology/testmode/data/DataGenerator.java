274ab39c9b084033b98c974093c5811fee84b4eb
sanyaminkin <sanyaminkin@gmail.com>
2023-03-01 10:59:42 +0300

    DataGenerator.java and AuthTest.java modified

--- src/test/java/ru/netology/testmode/data/DataGenerator.java
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -5,12 +5,14 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import lombok.Data;
 import lombok.Value;
-import lombok.val;
 
 import java.util.Locale;
 
 import static io.restassured.RestAssured.given;
+
+@Data
 
 public class DataGenerator {
     private static final RequestSpecification requestSpec = new RequestSpecBuilder()
@@ -20,26 +22,27 @@ public class DataGenerator {
             .setContentType(ContentType.JSON)
             .log(LogDetail.ALL)
             .build();
+
     private static final Faker faker = new Faker(new Locale("en"));
 
     private DataGenerator() {
     }
 
     private static void sendRequest(RegistrationDto user) {
-        // TODO: отправить запрос на указанный в требованиях path, передав в body запроса объект user
-        //  и не забудьте передать подготовленную спецификацию requestSpec.
-        //  Пример реализации метода показан в условии к задаче.
+        given()
+                .spec(requestSpec)
+                .body(new RegistrationDto(user.getLogin(), user.getPassword(), user.getStatus()))
+                .when()
+                .post("/api/system/users");
     }
 
     public static String getRandomLogin() {
-        // TODO: добавить логику для объявления переменной login и задания её значения, для генерации
-        //  случайного логина используйте faker
+        String login = faker.cat().name();;
         return login;
     }
 
     public static String getRandomPassword() {
-        // TODO: добавить логику для объявления переменной password и задания её значения, для генерации
-        //  случайного пароля используйте faker
+        String password = faker.bothify("?#???#?#");
         return password;
     }
 
@@ -48,13 +51,15 @@ public class DataGenerator {
         }
 
         public static RegistrationDto getUser(String status) {
-            // TODO: создать пользователя user используя методы getRandomLogin(), getRandomPassword() и параметр status
+            String login = getRandomLogin();
+            String password = getRandomPassword();
+            var user = new RegistrationDto(login, password, status);
             return user;
         }
 
         public static RegistrationDto getRegisteredUser(String status) {
-            // TODO: объявить переменную registeredUser и присвоить ей значение возвращённое getUser(status).
-            // Послать запрос на регистрацию пользователя с помощью вызова sendRequest(registeredUser)
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
             return registeredUser;
         }
     }
--- src/test/java/ru/netology/testmode/test/AuthTest.java
+++ src/test/java/ru/netology/testmode/test/AuthTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
 import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
 import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
@@ -21,25 +23,32 @@ class AuthTest {
     @DisplayName("Should successfully login with active registered user")
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
+        $("[data-test-id= login]").$("[class= input__control]").setValue(registeredUser.getLogin());
+        $("[data-test-id= password]").$("[class= input__control]").setValue(registeredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[id = root]").shouldHave(exactText("Личный кабинет"));
     }
 
     @Test
     @DisplayName("Should get error message if login with not registered user")
     void shouldGetErrorIfNotRegisteredUser() {
         var notRegisteredUser = getUser("active");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+        $("[data-test-id= login]").$("[class= input__control]").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id= password]").$("[class= input__control]").setValue(notRegisteredUser.getPassword());
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id= error-notification]").shouldBe(visible).
+                shouldHave(exactText("Ошибка\n" + "Ошибка! Неверно указан логин или пароль"));
     }
 
     @Test
     @DisplayName("Should get error message if login with blocked registered user")
     void shouldGetErrorIfBlockedUser() {
         var blockedUser = getRegisteredUser("blocked");
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+//        $("[data-test-id= login]").$("[class= input__control]").setValue(registeredUser.getLogin());
+//        $("[data-test-id= password]").$("[class= input__control]").setValue(registeredUser.getPassword());
+//        $$("button").find(exactText("Продолжить")).click();
+//        $("[data-test-id= error-notification]").shouldBe(visible).
+//                shouldHave(exactText("Ошибка\n" + "Ошибка! Пользователь заблокирован"));
     }
 
     @Test
@@ -47,9 +56,12 @@ class AuthTest {
     void shouldGetErrorIfWrongLogin() {
         var registeredUser = getRegisteredUser("active");
         var wrongLogin = getRandomLogin();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
+        $("[data-test-id= login]").$("[class= input__control]").setValue(registeredUser.getLogin());
+        $("[data-test-id= password]").$("[class= input__control]").setValue(wrongLogin);
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id= error-notification]").shouldBe(visible).
+                shouldHave(exactText("Ошибка\n" + "Ошибка! Неверно указан логин или пароль"));
+
     }
 
     @Test
@@ -57,8 +69,10 @@ class AuthTest {
     void shouldGetErrorIfWrongPassword() {
         var registeredUser = getRegisteredUser("active");
         var wrongPassword = getRandomPassword();
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
+        $("[data-test-id= login]").$("[class= input__control]").setValue(registeredUser.getLogin());
+        $("[data-test-id= password]").$("[class= input__control]").setValue(wrongPassword);
+        $$("button").find(exactText("Продолжить")).click();
+        $("[data-test-id= error-notification]").shouldBe(visible).
+                shouldHave(exactText("Ошибка\n" + "Ошибка! Неверно указан логин или пароль"));
     }
 }
